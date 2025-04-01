2cc54bcb9956a76cf9cc4194f433ab36a59ae615
Lyric's <riricoplay@hotmail.com>
2025-04-01 22:16:03 +0200

    update themes

--- app/src/main/java/iut/dam/projetsaedon/associations/AssociationDetailActivity.java
+++ app/src/main/java/iut/dam/projetsaedon/associations/AssociationDetailActivity.java
@@ -17,10 +17,10 @@ public class AssociationDetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_association_detail);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
     }
 }
